139568c7609f655da1829ebd02bdc2342fc5ae9c
Farjana Huq <farjanahuq@Farjanas-MacBook-Pro.local>
2018-11-13 14:06:29 -0800

    FINAL UPDATE

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,59 +5,79 @@ var displayScoreElem = $("#display-score");
 var winsCounterElem = $("#wins");
 var losesCounterElem = $("#loses");
 
-var randomGuess = 0 ;
+var randomGuess = 0;
 var wins = 0;
 var loses = 0;
 var totalScore = 0;
 
-var resetOrStartOverTheGame = function(){
+var resetOrStartOverTheGame = function () {
 
-$(".crystals").empty();
-$("#display-score").empty();
+    $(".crystals").empty();
+    $("#display-score").empty();
 
-randomGuess = Math.floor(Math.random()*(120-19+1)) + 19;
-console.log(randomGuess);
-randomGuessElem.text(randomGuess);
+    randomGuess = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    console.log(randomGuess);
+    randomGuessElem.text(randomGuess);
 
-// Generate random number of the 4 crystals between 1 and 12
+    var images = [
+        "./assets/images/diamond.png",
+        "./assets/images/emerald.png",
+        "./assets/images/ruby.png",
+        "./assets/images/sapphire.png"
+    ];
 
+    // Generate random number of the 4 crystals between 1 and 12
+    for (var i = 0; i < 4; i++) {
 
-for(var i = 0; i<4; i++){
-    var randomCrystal = Math.floor(Math.random()*12)+1;
-    console.log(randomCrystal);
-    var crystal = $("<div>");
-    crystal.attr({
-        "class": "crystal",
-        "random-data": randomCrystal
-    });
-    $(".crystals").append(crystal);
-   }
-}
-//Start the game
-resetOrStartOverTheGame();
+        var randomCrystal = Math.floor(Math.random() * 12) + 1;
+        console.log(randomCrystal);
 
-// Onclick function for each crystal
-// each crystal click will add numbers to the total score
-$(document).on("click", ".crystal",function(){
+        var crystal = $("<div>");
+        var image = $("<img>");
+        
+        crystal.attr({
+            "class": "crystal",
+            "random-data": randomCrystal,
+            "images": images[i]
+        });
+        //console.log( $(this).attr("images"));
+       
+        $(".crystals").append(crystal);
+     
+        crystal.css({
+              "background-image": "url(' " + images[i] + " ')",
+              "background-size" : "cover"
+        });
+      }
+    }
+
+    //Start the game
+    resetOrStartOverTheGame();
+
+   // Onclick function for each crystal
+   // each crystal click will add numbers to the total score
+
+   $(document).on("click", ".crystal", function () {
+
     var num = parseInt($(this).attr("random-data"));
-   // console.log("CrystalClick: " + num);
-   totalScore += num;
-   console.log("Total Score : " + totalScore);
-   displayScoreElem.text(totalScore);
-   
-   if(totalScore == randomGuess){
-       wins++;
-       alert("You Win!");
-       totalScore = 0;
-       resetOrStartOverTheGame();
-   }else if(totalScore > randomGuess){
-       loses++;
-       alert("You Loose!");
-       totalScore = 0;
-       resetOrStartOverTheGame();
-   }
-   winsCounterElem.text(wins);
-   losesCounterElem.text(loses);
+    // console.log("CrystalClick: " + num);
+    totalScore += num;
+    console.log("Total Score : " + totalScore);
+    displayScoreElem.text(totalScore);
+
+    if (totalScore == randomGuess) {
+        wins++;
+        alert("You Win!");
+        totalScore = 0;
+        resetOrStartOverTheGame();
+    } else if (totalScore > randomGuess) {
+        loses++;
+        alert("You Loose!");
+        totalScore = 0;
+        resetOrStartOverTheGame();
+    }
+    winsCounterElem.text(wins);
+    losesCounterElem.text(loses);
 })
 
 // check if the total score is equal to the random number or not
